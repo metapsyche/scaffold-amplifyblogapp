a478b563aed696183871e423048b553d16092137
Metapsyche <mindrogen@gmail.com>
2022-05-06 11:38:43 -0400

    added latest amplify pull

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+
+import { 
+  SocialPostCollection 
+} from './ui-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyProvider>
+      <SocialPostCollection />
+    </AmplifyProvider>
   );
 }
 
